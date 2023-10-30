a2893e537021dd3e9c5087be718f556a02c9e2e8
Ivan Sheva <sevcenkoivan673@gmail.com>
2023-10-30 11:22:50 +0200

    Add counter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { useState } from "react";
+
 function App() {
+  const [counter, setCounter] = useState(0);
   return (
     <div className="App">
-      <button>
-
-      </button>
+      <h1>Value: {counter}</h1>
+      <button onClick={setCounter((prev) => prev + 1)}>Increment</button>
     </div>
   );
 }
