417a295919cf11167f8d49cffa4e7a0710bbe2bb
Ivan Sheva <sevcenkoivan673@gmail.com>
2023-10-30 11:30:48 +0200

    Fix counter bug

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Value: {counter}</h1>
-      <button onClick={setCounter((prev) => prev + 1)}>Increment</button>
+      <button onClick={() => setCounter((prev) => prev + 1)}>Increment</button>
     </div>
   );
 }
